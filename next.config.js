ae3d30a6c1995f45fd0f9d394563f12337fac4a1
jonas souza <jonassouza305@yahoo.com.br>
2025-12-09 12:36:57 -0300

    feat: Change project hosting to GitHub Actions.

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/logistics-tracking",
+  assetPrefix: "/logistics-tracking/",
+};
+
+module.exports = nextConfig;
